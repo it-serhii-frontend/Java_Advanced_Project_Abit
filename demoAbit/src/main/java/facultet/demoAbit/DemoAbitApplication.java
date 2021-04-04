4391ae01cb7d1f68b42bb93af9d834cf7dbf30f0
Serhii <sergonv077@gmail.com>
2021-04-04 20:35:27 +0300

    add domain models

---
+++ demoAbit/src/main/java/facultet/demoAbit/DemoAbitApplication.java
@@ -0,0 +1,13 @@
+package facultet.demoAbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAbitApplication.class, args);
+	}
+
+}
---
+++ demoAbit/src/test/java/facultet/demoAbit/DemoAbitApplicationTests.java
@@ -0,0 +1,13 @@
+package facultet.demoAbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
